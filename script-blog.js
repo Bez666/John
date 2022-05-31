f53cada6d7c982117642df843e26b80e701ec480
Bez666 <106593258+Bez666@users.noreply.github.com>
2022-05-31 16:53:47 +0500

    Delete script-blog.js

--- script-blog.js
+++
@@ -1,6 +0,0 @@
-const headerLinkMain = document.querySelector('.header__link').nextElementSibling;
-const headerLink = headerLinkMain.onclick = click();
-
-function click() {
-    headerLinkMain.classList.toggle('active');
-}
