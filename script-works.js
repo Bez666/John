52e284a66b20cb41c50919db60654adf6c8e3cd5
Bez666 <106593258+Bez666@users.noreply.github.com>
2022-05-31 16:53:57 +0500

    Delete script-works.js

--- script-works.js
+++
@@ -1,6 +0,0 @@
-const headerLinkMain = document.querySelector('.header__link');
-const headerLink = headerLinkMain.onclick = click();
-
-function click() {
-        headerLinkMain.classList.toggle('active');
-    }
